b573ac4924ae89726525f2972aa630399976ab3e
Trunks @ Carbonable <tarik.kaced@protonmail.com>
2024-09-08 20:49:14 +0200

    Feat: Strat algo (#6)
    * ✨ strat algo
    
    * ✨ add budget to input
    
    * ✨ add strat algo to api result
    
    ---------
    
    Co-authored-by: julienbrs <julien.bourseau@gmail.com>

--- src/pages/api/run-algo.ts
+++ src/pages/api/run-algo.ts
@@ -1,39 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { runBudgetAlgorithm } from '@/algorithms/algoBudget';
+<<<<<<< HEAD
+import { runStratAlgorithm } from '@/algorithms/algoStrat';
+=======
 import { configMap } from '@/constants/configs';
 import { ConfigType } from '../input-form-typo';
 import { runTypoAlgorithm } from '@/algorithms/algoType';
 import { AlgorithmInput, ProjectConfig, RegionAllocation, TimeConstraint } from '@/types';
+>>>>>>> main
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
-      const input = req.body as AlgorithmInput;
-      let algoRes;
-      let projectConfig: ProjectConfig[] = [];
+      const input = req.body;
 
-      if ('budget' in input && 'configType' in input) {
-        // It's a TypoAlgorithmInput
-        projectConfig = configMap[input.configType as ConfigType];
-        algoRes = runTypoAlgorithm({
-          regionAllocation: input.regionAllocation,
-          projectConfig,
-          budget: input.budget,
-          financing: input.financing,
-          timeConstraints: input.timeConstraints,
-        });
-      } else if ('typology' in input) {
-        // It's a BudgetAlgorithmInput
-        algoRes = runBudgetAlgorithm({
-          regionAllocation: input.regionAllocation,
-          typology: input.typology,
-          financing: input.financing,
-          timeConstraints: input.timeConstraints,
-        });
-      } else {
-        throw new Error('Invalid input type');
-      }
-
+      // Run the algorithm with the provided input
+      console.log('\n\n\n\n');
+      console.log('\x1b[36m%s\x1b[0m', '#############################');
+      console.log('Running algorithm with input:', input);
+      const algoRes = runBudgetAlgorithm({
+        regionAllocation: input.regionAllocation,
+        typology: input.typology,
+        financing: input.financing,
+        timeConstraints: input.timeConstraints,
+      });
       if (!algoRes) {
         throw new Error('An error occurred while running the algorithm.');
       }
@@ -41,7 +31,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const totalBudget = algoRes;
 
       // Respond with the result
+<<<<<<< HEAD
+      res.status(200).json({
+        budget: { totalBudget, adjustedBudget },
+        strat: { nbsRemoval, nbsAvoidance, biochar, dac, stratAdjustedBudget, carbonToOffset }
+      });
+=======
       res.status(200).json(totalBudget);
+>>>>>>> main
     } catch (error) {
       console.error('Error running algorithm:', error);
       res.status(500).json({ error: 'An error occurred while running the algorithm.' });
