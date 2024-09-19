7808872fbdf1d25abde24ef946c1c7519dac0c52
tekkac <tarik@carbonable.io>
2024-09-19 18:20:00 +0200

    ✨ introduce tips in budget algo

--- src/actions/budget.ts
+++ src/actions/budget.ts
@@ -1,14 +1,19 @@
 'use server';
 
+import { adviceBudgetTimeline } from '@/algorithms/advice/budgetEstimationAdvice';
 import { runBudgetAlgorithm } from '@/algorithms/algoBudget';
 import { BudgetAlgorithmInput, BudgetOutputData } from '@/types/types';
 
 export async function runBudgetAlgo(input: BudgetAlgorithmInput): Promise<BudgetOutputData> {
-  const algoRes: BudgetOutputData = runBudgetAlgorithm(input);
+  let algoRes: BudgetOutputData = runBudgetAlgorithm(input);
 
   if (!algoRes) {
     throw new Error('An error occurred while running the algorithm.');
   }
+
+  let timelineTip = adviceBudgetTimeline(input, algoRes);
+  console.log(timelineTip);
+  algoRes = { ...algoRes, advice_timeline: timelineTip.advice };
 
   return algoRes;
 }
