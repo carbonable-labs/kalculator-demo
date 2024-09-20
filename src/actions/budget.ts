a765a6ea10c5c7c978e1453499860028cfcdd4ea
tekkac <tarik@carbonable.io>
2024-09-20 16:59:15 +0200

    ✨ integrate computed advice

--- src/actions/budget.ts
+++ src/actions/budget.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { adviceBudgetTimeline } from '@/algorithms/advice/budgetEstimationAdvice';
+import { Advice, computeBudgetAdvice } from '@/algorithms/advice/budgetEstimationAdvice';
 import { runBudgetAlgorithm } from '@/algorithms/algoBudget';
 import { BudgetAlgorithmInput, BudgetOutputData } from '@/types/types';
 
@@ -11,18 +11,31 @@ export async function runBudgetAlgo(input: BudgetAlgorithmInput): Promise<Budget
     throw new Error('An error occurred while running the algorithm.');
   }
 
-  let timelineTip = adviceBudgetTimeline(input, algoRes);
-  console.log(timelineTip);
-  let advice: string;
-  if (!timelineTip.change) {
-    advice = timelineTip.advice;
-  } else {
-    let phrase = timelineTip.tip ? timelineTip.tip.advicePhrase : '';
-    let option = timelineTip.tip ? timelineTip.tip.smartTip : '';
-    let delta = timelineTip.tip ? timelineTip.tip.budgetDelta : '';
-    advice = `${phrase} ${option} and save $${delta}`;
-  }
-  algoRes = { ...algoRes, advice_timeline: advice };
+  let computedAdvice = computeBudgetAdvice(input, algoRes);
+  console.log(computedAdvice);
+
+  let formattedTips = computedAdvice.map(formatTip);
+
+  console.log(formattedTips);
+  algoRes = {
+    ...algoRes,
+    advice_timeline: formattedTips[0],
+    advice_financing: formattedTips[1],
+    advice_typo: formattedTips[2],
+    advice_geography: formattedTips[3],
+  };
 
   return algoRes;
+}
+
+function formatTip(someAdvice: Advice): string {
+  let result: string = '';
+  if (someAdvice.change) {
+    let someTip = someAdvice.tip;
+    let phrase = someTip ? someTip.advicePhrase : '';
+    let option = someTip ? someTip.smartTip : '';
+    let delta = someTip ? someTip.budgetDelta : '';
+    result = `${phrase} ${option} and save $${delta}`;
+  }
+  return result;
 }
