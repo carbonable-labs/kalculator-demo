56aeabce158aadd3f97505f7407583420d226168
tekkac <tarik@carbonable.io>
2024-09-27 17:11:23 +0200

    🩹 fix default suggested config

--- src/utils/configuration.ts
+++ src/utils/configuration.ts
@@ -9,8 +9,8 @@ export enum TYPOLOGY_PREFERENCE {
 }
 
 export const DEFAULT_FINANCING: Financing = {
-  financingExAnte: 0.88,
-  financingExPost: 0.12,
+  financingExAnte: 0.5,
+  financingExPost: 0.5,
 };
 
 export const DEFAULT_TYPOLGY: Typology = {
@@ -32,43 +32,43 @@ export const DEFAULT_GEOGRAPHICAL_AREA = {
 export const CLIMATE_AVOIDANCE_TYPOLGY: Typology = {
   nbsRemoval: 0.1,
   nbsAvoidance: 0.2,
-  biochar: 0.5,
   dac: 0.2,
+  biochar: 0.5,
 };
 
 export const CLIMATE_REMOVAL_TYPOLGY: Typology = {
   nbsRemoval: 0.2,
   nbsAvoidance: 0.1,
-  biochar: 0.5,
   dac: 0.2,
+  biochar: 0.5,
 };
 
 export const CLIMATE_NO_IDEA_TYPOLGY: Typology = {
   nbsRemoval: 0.15,
   nbsAvoidance: 0.15,
-  biochar: 0.5,
   dac: 0.2,
+  biochar: 0.5,
 };
 
 export const BIODIVERSITY_NO_IDEA_TYPOLGY: Typology = {
   nbsRemoval: 0.4,
-  nbsAvoidance: 0.4,
+  nbsAvoidance: 0.3,
+  dac: 0.1,
   biochar: 0.2,
-  dac: 0,
 };
 
 export const BIODIVERSITY_AVOIDANCE_TYPOLGY: Typology = {
   nbsRemoval: 0.2,
   nbsAvoidance: 0.6,
-  biochar: 0.2,
   dac: 0,
+  biochar: 0.2,
 };
 
 export const BIODIVERSITY_REMOVAL_TYPOLGY: Typology = {
   nbsRemoval: 0.6,
-  nbsAvoidance: 0.2,
+  nbsAvoidance: 0.0,
+  dac: 0.2,
   biochar: 0.2,
-  dac: 0,
 };
 
 export const continents = ['africa', 'asia', 'europe', 'north_america', 'oceania', 'south_america'];
