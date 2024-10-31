00f3215987a915669b36f0a9e93f1eee8b9f9401
François Schuers <francois.schuers@gmail.com>
2024-10-31 14:57:24 +0100

    fix: typo

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -4,7 +4,7 @@ export default function Title() {
   return (
     <div className="mt-4 flex items-center justify-center">
       <Image src="/assets/logo.svg" alt="Logo" width={80} height={80} />
-      <div className="ml-1 text-4xl font-bold text-neutral-50">ALCULTATOR</div>
+      <div className="ml-1 text-4xl font-bold text-neutral-50">ALCULATOR</div>
     </div>
   );
 }
